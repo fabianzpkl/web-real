0a30eb39ae7afc0e85524d3ed5f8af1811a542c1
Fabian Zapata <fabian.zapata@pklagencia.com>
2023-09-21 22:15:38 -0500

    organizando menu y logo del header

--- header.php
+++ header.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
-    <head>
-        
-    </head>
+    <header>
+        <div class="cont-header">
+            <div class="logo">
+                <a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" alt="Real - Hotel & Resourts">
+                </a>
+            </div>
+            <div class="cont-menu">
+                <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'primary',
+                ));
+                ?>
+            </div>
+        </div>
+    </header>
