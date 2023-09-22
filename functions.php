11ee1de99637eeb173e06a1a1e31f3a6e109e16b
Fabian Zapata <fabian.zapata@pklagencia.com>
2023-09-21 22:05:07 -0500

    llamada de js y css materialize

--- functions.php
+++ functions.php
@@ -5,12 +5,14 @@ function cargar_jquery()
 {
     wp_deregister_script('jquery');
     wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.6.0.min.js', array(), null, true);
+    wp_enqueue_script('materialize', '/assets/js/materialize.min.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'cargar_jquery');
 
 
 function cargar_estilos()
 {
+    wp_enqueue_style('estilos-materialize', get_template_directory_uri() . '/assets/css/materialize.min.css');
     wp_enqueue_style('estilos-personalizados', get_template_directory_uri() . '/assets/css/main.css');
 }
 add_action('wp_enqueue_scripts', 'cargar_estilos');
