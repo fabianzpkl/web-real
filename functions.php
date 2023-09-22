a88ea80c6c931adbbb4c035b8c52581619c75670
Fabian Zapata <fabian.zapata@pklagencia.com>
2023-09-21 22:06:47 -0500

    Corrección url js materialize

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function cargar_jquery()
 {
     wp_deregister_script('jquery');
     wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.6.0.min.js', array(), null, true);
-    wp_enqueue_script('materialize', '/assets/js/materialize.min.js', array(), null, true);
+    wp_enqueue_script('materialize', get_template_directory_uri() . '/assets/js/materialize.min.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'cargar_jquery');
 
